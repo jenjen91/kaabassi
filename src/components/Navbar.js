486018a9e44b0a0c0e54208fb01138fb9b872bf4
Jenny M H Thorbjørnsen <jmthorbj@gmail.com>
2018-12-01 02:12:50 +0100

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/kaabassi-logo.png'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item" title="Logo">
-          <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+          <img src={logo} alt="Kaldi" style={{ width: '60px', maxHeight: 'none' }} />
         </Link>
       </div>
       <div className="navbar-start">
+        <Link className="navbar-item nav-home" to="/">
+          <b>Stichting Kaabassi</b>
+        </Link>
         <Link className="navbar-item" to="/about">
-          About
+          Over Ons
         </Link>
         <Link className="navbar-item" to="/products">
-          Products
+          Projeecten
         </Link>
         <Link className="navbar-item" to="/contact">
           Contact
@@ -26,16 +28,7 @@ const Navbar = () => (
         </Link>
       </div>
       <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
+
       </div>
     </div>
   </nav>
